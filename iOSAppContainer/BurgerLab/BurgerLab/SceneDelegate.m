9bbf6bba675ab7ba3a50f2c875b599df2d88952b
jgch88 <radium-laboratories@gmail.com>
2019-12-15 18:12:55 +0800

    created new Podfile for AppContainer's workspace, build AppContainer with RN Framework. crashing on missing bundle

--- iOSAppContainer/BurgerLab/BurgerLab/SceneDelegate.m
+++ iOSAppContainer/BurgerLab/BurgerLab/SceneDelegate.m
@@ -1,5 +1,6 @@
 #import "SceneDelegate.h"
 #import "ViewController.h"
+#import <BurgerLabGameFramework/BurgerLabGameViewController.h>
 
 @interface SceneDelegate ()
 
@@ -10,8 +11,12 @@
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
     
+    BurgerLabGameViewController *reactNativeViewController = [BurgerLabGameViewController new];
+    reactNativeViewController.title = @"Game";
+    reactNativeViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Game" image:[UIImage systemImageNamed:@"circle.grid.hex"] tag:1];
+    
     UITabBarController *tabBarController = [[UITabBarController alloc] initWithNibName:nil bundle:nil];
-    tabBarController.viewControllers = [[NSArray alloc] initWithObjects: nil];
+    tabBarController.viewControllers = [[NSArray alloc] initWithObjects:reactNativeViewController, nil];
     
     self.window.rootViewController = tabBarController;
     [self.window makeKeyAndVisible];
