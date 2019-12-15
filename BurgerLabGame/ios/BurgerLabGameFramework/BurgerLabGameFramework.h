ab73b1a47ee545cb40cead52bee062f0d7d43a8f
jgch88 <radium-laboratories@gmail.com>
2019-12-15 16:59:23 +0800

    add new build target (Framework) for BurgerLabGame

---
+++ BurgerLabGame/ios/BurgerLabGameFramework/BurgerLabGameFramework.h
@@ -0,0 +1,19 @@
+//
+//  BurgerLabGameFramework.h
+//  BurgerLabGameFramework
+//
+//  Created by JUSTIN GOH on 15/12/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BurgerLabGameFramework.
+FOUNDATION_EXPORT double BurgerLabGameFrameworkVersionNumber;
+
+//! Project version string for BurgerLabGameFramework.
+FOUNDATION_EXPORT const unsigned char BurgerLabGameFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BurgerLabGameFramework/PublicHeader.h>
+
+
